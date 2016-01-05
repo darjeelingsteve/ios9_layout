affd1d74e335b706e621ee78959c8fec6e11aa5d
Stephen Anthony <steve@darjeelingapps.com>
2016-01-05 10:46:06 +0000

    Initial Commit

---
+++ iOS9 Layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS9 Layout
+//
+//  Created by Stephen Anthony on 05/01/2016.
+//  Copyright © 2016 Darjeeling Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
